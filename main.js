e6ccc91bb5219f775241072f1c3584506173dd7f
alawld <1679748907@qq.com>
2018-02-01 16:12:56 +0800

    添加页面visibilitychange时的操作

--- main.js
+++ main.js
@@ -2,10 +2,25 @@ let $images = $("#images")
 let $buttons = $("#buttons > button")
 let $imagesChildren = $images.children('img')
 let current = 0
+let timer = {}
 
 makeFakeImage()
 $('#images').css({transform: `translateX(-400px)`})
 bindEvents()
+autoSlide()
+
+function autoSlide(){
+    timer = setInterval(()=>{
+        goSlide(current + 1)
+    }, 3000)
+    $('#container').on('mouseenter', ()=>{
+        window.clearInterval(timer)
+    }).on('mouseleave', ()=>{
+        timer = setInterval(()=>{
+        goSlide(current + 1)
+    }, 3000)
+    })
+}
 
 function bindEvents(){
     // TODO why + button?
@@ -15,26 +30,24 @@ function bindEvents(){
         goSlide(index)
     })
     $('#previous').on('click', function(){
-        if(current === 0){
-            goSlide($imagesChildren.length -1)
-        }else{
-            goSlide(current - 1) 
-        }
+        goSlide(current - 1)
     })
     $('#next').on('click', function(){
-        if(current === $imagesChildren.length -1){
-            goSlide(0)
-        }else{
-            goSlide(current + 1)
-        }
+        goSlide(current + 1)
     })
 }
 
+
+
 function goSlide(index){
-    console.log('c='+current + ' i='+index )
-    if(current === index){
-        return;
-     }else if(current === 0 && index === $imagesChildren.length -1){
+    if(current === index)return;
+    if(index > $imagesChildren.length -1){
+        index = 0
+    }else if(index < 0){
+        index = $imagesChildren.length -1
+    }
+
+    if(current === 0 && index === $imagesChildren.length -1){
     // 第一张 >> 最后一张 
         $('#images').css({transform: `translateX(0px)`})
         .one(`transitionend`, function(){
@@ -62,3 +75,25 @@ function makeFakeImage(){
     $images.append($firstcopy)
     $images.prepend($lastcopy)
 }
+
+var hidden, visibilityChange; 
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+
+document.addEventListener("visibilitychange", function() {
+    if(document[hidden]){
+        window.clearInterval(timer)
+    }else{
+        timer = setInterval(()=>{
+        goSlide(current + 1)
+        }, 3000)
+    }
+});
